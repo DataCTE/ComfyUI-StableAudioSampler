7af840ed7b0724829f7046eb5c64725b4c5a0281
Alexander R Izquierdo <105170707+DataCTE@users.noreply.github.com>
2024-06-06 07:01:00 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@
 @description: A Simple integration of Stable Audio Diffusion with knobs and stuff!
 """
 
-from .nodes import StableAudioSampler, NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
 
